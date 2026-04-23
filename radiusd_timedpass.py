dca4d09bb99acb000a7a415e442689ba95cde94d
Timo Makinen <tmakinen@foo.sh>
2026-04-23 16:33:44 +0000

    Fix formatting

--- radiusd_timedpass.py
+++ radiusd_timedpass.py
@@ -36,8 +36,11 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         super().__init__()
 
     def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
